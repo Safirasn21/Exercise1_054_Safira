f1891b7d99172cff5a0e2480a1e6187f76543061
safirasn21 <safirasn021@gmai.com>
2024-04-01 00:02:05 +0700

    first commit

---
+++ descButton.js
@@ -0,0 +1,11 @@
+function showProfile() {
+  window.open("https://kprofiles.com/boynextdoor-members-profile/", "_blank");
+}
+
+function redirectToYoutube(url) {
+  window.open(url, "_blank");
+}
+
+function showAlert() {
+  alert("Earth, Wind, & Fire is coming soon on 15 April 2024!");
+}
